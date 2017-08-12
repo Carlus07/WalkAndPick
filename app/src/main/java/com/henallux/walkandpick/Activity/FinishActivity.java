5e092fb714664f8dee17f4c61187a117823d48a6
Carlus <carlmath@hotmail.com>
2017-08-12 19:53:14 +0200

    Fin de la vue Finish

--- app/src/main/java/com/henallux/walkandpick/Activity/FinishActivity.java
+++ app/src/main/java/com/henallux/walkandpick/Activity/FinishActivity.java
@@ -1,15 +1,31 @@
 package com.henallux.walkandpick.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.henallux.walkandpick.R;
 
 public class FinishActivity extends AppCompatActivity {
 
+    Button Button_GOMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log);
+        setContentView(R.layout.activity_finish);
+
+        Button_GOMenu = (Button) findViewById(R.id.goMenu);
+        Button_GOMenu.setOnClickListener(GoMenu);
+
     }
+    private View.OnClickListener GoMenu = new View.OnClickListener(){
+        @Override
+        public void onClick(View V)
+        {
+            startActivity(new Intent(FinishActivity.this, MainActivity.class));
+        }
+    };
 }
